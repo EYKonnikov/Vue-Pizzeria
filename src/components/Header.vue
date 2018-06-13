dc59b34d104147c29773fc6c87002822754c3e44
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2018-06-13 22:35:58 +0200

    create components

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="row">
+    <nav class="navbar navbar-toggleable-md navbar-light">
+      <a class="navbar-brand" href="/">Pizzeria</a>
+      <ul class="nav navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="">home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">menu</a>
+        </li>
+      </ul>
+    </nav>
+
+  </header>
+</template>
+
+<style>
+  header {
+    margin-top: 15px;
+  }
+
+  .navbar-brand {
+    font-size: 1.5em;
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <div id="main" class="col-sm-12 text-center">
+      <div class="background">
+        <h1 class="main__title">Welcome to Pizzeria!</h1>
+        <h2 class="main__paragraph">Feeling hungry?</h2>
+        <button class="btn btn-success">Let's order!</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  #main {
+    height: 800px;
+    padding-top: 10%;
+    background: url('../../src/assets/background__image--pizza.jpg');
+    background-size: 100%;
+    background-repeat: no-repeat;
+  }
+
+
+  .main__title,
+  .main__paragraph {
+    margin-bottom: 6%;
+  }
+
+  .background {
+    max-width: 60%;
+    margin: 0 auto;
+    background: #eee;
+    opacity: 0.8;
+    padding: 20px 0;
+  }
+
+  
+  
+</style>
+
