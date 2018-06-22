0ef24ae72339a966250324ecac8d3d54c3f09620
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2018-06-22 16:17:32 +0200

    Making the shopping basket dynamic

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -21,7 +21,10 @@
         </tbody>
       </table>
     </div>
+
+    <!-- shopping basket -->
     <div class="col-sm-12 col-md-6">
+    <div v-if="basket.length > 0">
        <table class="table">
         <thead class="thead-dark">
           <tr>
@@ -30,20 +33,24 @@
             <th>Total</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody v-for="item in basket">
           <tr>
             <td>
               <button class="btn btn-sm" type="button">-</button>
-              <span>1-</span>
+              <span>{{ item.quantity }}-</span>
               <button class="btn btn-sm" type="button">+</button>
             </td>
-            <td>Margherita 9"</td>
-            <td>9.95</td>
+            <td>{{ item.name }} {{ item.size }}</td>
+            <td>{{ item.price * item.quantity }}</td>
           </tr>
         </tbody>
       </table>
       <p>Order total: </p>
       <button class="btn btn-success btn-block">Place Order</button>
+    </div>
+    <div v-else>
+      <p>{{ basketText }}</p>
+    </div>
     </div>
   </div>
 </template>
@@ -54,6 +61,7 @@ export default {
   data() {
     return {
       basket: [],
+      basketText: 'Your basket is empty!',
       getMenuItems: {
         1: {
           name: "Margherita",
