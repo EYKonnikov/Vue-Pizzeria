65ee05b6f69575ecb70113e4c5c7ab50f7972daf
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2018-07-04 19:03:59 +0200

    Signing in and out

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,8 +9,8 @@
         <label>Password</label> 
         <input class="form-control" type="password" id="password" placeholder="Enter password">
     </div>
-    <button type="button" class="btn btn-primary" @click.prevent="SignIn">Sign in</button>
-    <button type="button" class="btn btn-danger" @click.prevent="SignOut">Sign out</button>
+    <button type="button" class="btn btn-primary" @click.prevent="signIn">Sign in</button>
+    <button type="button" class="btn btn-danger" @click.prevent="signOut">Sign out</button>
     </form>
   </div>
 </template>
@@ -21,10 +21,26 @@ import Firebase from 'firebase'
 export default {
   methods: {
     signIn() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
 
-    },
+      Firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if(errorCode === 'auth/wrond-password') {
+          alert('Wrond password');
+        } else {
+          alert(errorMessage);
+        }
+      });
+      },
     signOut() {
-      
+      Firebase.auth().signOut().then(function() {
+        alert('logged out');
+      }).catch(function(error) {
+        alert("error");
+      })
     }
   }
 }
