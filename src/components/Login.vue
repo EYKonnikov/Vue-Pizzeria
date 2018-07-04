488ad154a081be1993cd210178270635cb69cf12
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2018-07-04 15:41:03 +0200

    Enable Firebase authentication

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,8 +9,25 @@
         <label>Password</label> 
         <input class="form-control" type="password" id="password" placeholder="Enter password">
     </div>
-    <button type="button" class="btn btn-primary">Sign in</button>
-    <button type="button" class="btn btn-danger">Sign out</button>
+    <button type="button" class="btn btn-primary" @click.prevent="SignIn">Sign in</button>
+    <button type="button" class="btn btn-danger" @click.prevent="SignOut">Sign out</button>
     </form>
   </div>
 </template>
+
+<script>
+import Firebase from 'firebase'
+
+export default {
+  methods: {
+    signIn() {
+
+    },
+    signOut() {
+      
+    }
+  }
+}
+
+</script>
+
