f75637054e26f23dae37e46bdb89addf57e89de9
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2018-07-05 17:40:29 +0200

    Final components

---
+++ src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Contact</h5>
+    <div class="card-text">
+      <address>
+        <strong>Pizzeria</strong><br>
+        Adress line 1<br>
+        Adress line 2<br>
+        Phone: (123) 456-7890
+      </address>
+      <address>
+        <strong>Email:</strong><br>
+        <p>contact@pizzeria.com</p>
+      </address>
+    </div>
+  </div>
+</div>
+</template>
