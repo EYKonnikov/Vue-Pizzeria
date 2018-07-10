d42cf0b2eb987e207e375518f505947a5805d0ba
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2018-07-10 18:47:24 +0200

    Router links and bindings

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,16 +3,25 @@
     <nav class="navbar navbar-expand-sm navbar-light">
       <a class="navbar-brand" href="/">PIZZERIA</a>
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link to="/"><a class="nav-link">home</a></router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/menu"><a class="nav-link">menu</a></router-link>
-        </li>
+        <router-link :to="homeLink" tag="li"><a class="nav-link">home</a></router-link>
+        <router-link :to="menuLink" tag="li"><a class="nav-link">menu</a></router-link>
       </ul>
     </nav>
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      homeLink: '/',
+      menuLink: '/menu'
+    }
+  }
+}
+</script>
+
+
 
 <style>
   header {
